ea3bb50c603d4c464237a1043b1c1f1c35e5026d
suresh <sureshm434@gmail.com>
2023-05-06 19:34:00 +0530

    cucumber report generated screenshot of failed test attached to report

--- src/test/java/hooks/MyHooks.java
+++ src/test/java/hooks/MyHooks.java
@@ -3,12 +3,15 @@ package hooks;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import factory.Driverfactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import utils.ConfigReader;
 
 public class MyHooks {
@@ -23,7 +26,15 @@ WebDriver driver;
 	}
 
 	@After
-	public void tearDown() {
+	public void tearDown(Scenario scenario) {
+		
+		String scenarioName = scenario.getName().replace(" ", "_");
+		
+		if(scenario.isFailed()) {
+			byte[] srcScreeenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(srcScreeenshot,"image/png", scenarioName);
+		}
+		
 		driver.quit();
 	}
 }
--- src/test/java/runner/MyRunner.java
+++ src/test/java/runner/MyRunner.java
@@ -7,7 +7,9 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources",
-					glue= {"stepdefinitions","hooks"})
+					glue= {"stepdefinitions","hooks"},
+					publish= true,
+					plugin={"pretty","html:target/CucumberReports/CucumberReport.html"})
 public class MyRunner {
 
 	
